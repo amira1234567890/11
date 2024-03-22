1c8c51efe30c617d06b2c3f2d7a9dce035839f4f
Amira Natasha Binti Abdullah Harun <amiranatashaabdharun@gmail.com>
2024-03-22 08:12:00 +0000

    tes

---
+++ ConsoleApp31/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Class1
+    {
+        private int v;
+
+        public Class1(int v)
+        {
+            this.v = v;
+        }
+
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+    }
+
+    public class Class2
+    {
+        Class1 obj = new(2);
+        public Class2(int v) {
+        }
+
+        public class Class3
+        {
+            Class1 obj = new(3);
+    }
+    }
+}
---
+++ ConsoleApp31/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    public class Class4
+    {
+        Class1 obj = new(1);
+    }
+}
