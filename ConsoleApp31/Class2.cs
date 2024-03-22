898de49b01edcd7daa46374ec9a1eee4341577c0
Amira Natasha Binti Abdullah Harun <amiranatashaabdharun@gmail.com>
2024-03-22 08:14:16 +0000

    a

--- ConsoleApp31/Class2.cs
+++ ConsoleApp31/Class2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,10 @@ namespace ConsoleApp31
     public class Class4
     {
         Class1 obj = new(1);
+        Print obj2 = new();
+    }
+
+    internal class Print
+    {
     }
 }
